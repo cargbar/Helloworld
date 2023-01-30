232042231e371e80f2f3f6aba1fa86c0fdcf2c89
cargbar93 <c.argbar2393@gmail.com>
2023-01-29 21:00:05 -0600

    Coments

--- Tarea_2/factorial_de_numero.py
+++ Tarea_2/factorial_de_numero.py
@@ -1,8 +1,10 @@
-# 
+# Solicitar variable de entrada
 num =int(input("Igresar un numero entero:"))
+# Validacion
 if num >= 0:
     control_value=1
     iter_value=1
+    # Factorial del numero
     while control_value <= num:
         iter_value = iter_value * control_value
         control_value += 1
--- Tarea_2/triangulo_numero.py
+++ Tarea_2/triangulo_numero.py
@@ -1,5 +1,8 @@
+# Solicitar variable de  entrada
 num =int(input("Igresar un numero entero: "))
+# Validacion
 if num>0:
+# Funcion triangulo numerico    
     for i in range(1,num+1):
         for j in range (1,i+1):
             print(j,end=" ")
