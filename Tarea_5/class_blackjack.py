a7aaf8847e20e4694a790a188ea28624ffe2edfc
Carlos Mario Arguedas Barahona <39394819+cargbar@users.noreply.github.com>
2023-02-26 18:49:52 -0600

    Tarea 5

---
+++ Tarea_5/class_blackjack.py
@@ -0,0 +1,68 @@
+# Importar modulos
+import random
+
+# Variables de juego
+suits = ("Hearts", "Diamonds", "Spades", "Clubs")
+ranks = (
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Jack",
+    "Queen",
+    "King",
+    "Ace",
+)
+values = {
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5,
+    "Six": 6,
+    "Seven": 7,
+    "Eight": 8,
+    "Nine": 9,
+    "Ten": 10,
+    "Jack": 10,
+    "Queen": 10,
+    "King": 10,
+    "Ace": 11,
+}
+playing = True
+
+
+# Clase Carta:
+class card:
+    def init(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def str(self):
+        return self.rank + "of" + self.suit
+
+
+# Clase Deck:
+class deck:
+    def init(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(card(suit, rank))
+
+    def str(self):
+        deck_comp = ""
+        for card in self.deck:
+            deck_comp += "\n" + card.str()
+        return "The deck has" + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
